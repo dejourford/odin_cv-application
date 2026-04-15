9c45fcb4e3d6dfd44862f8ccabdb3b6694dc124b
De'Jour <dejourford@yahoo.com>
2026-04-15 12:31:21 -0500

    create skeleton structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,61 @@
+import "./App.css"
+import { Input } from "./components/Input"
 
 
 function App() {
-  <>
-  </>
+  return (
+    <>
+      <h1>CV Application</h1>
+      <main>
+
+        {/* Personal Info Section */}
+        <section className="cv-section" id="personal">
+          <h2>Personal Information</h2>
+          <form>
+            <Input type={"text"} name={"name"} />
+            <Input type={"email"} name={"email"} />
+            <Input type={"number"} name={"phone"} />
+            <div className="button-group">
+              <button type="button">Edit</button>
+              <button type="button">Save</button>
+            </div>
+          </form>
+        </section>
+
+        {/* Education Section */}
+        <section className="cv-section" id="education">
+          <h2>Education Information</h2>
+          <form>
+            <Input type={"text"} name={"school name"} />
+            <Input type={"text"} name={"major"} />
+            <Input type={"number"} name={"year of graduation"} />
+            <div className="button-group">
+              <button type="button">Edit</button>
+              <button type="button">Save</button>
+            </div>
+          </form>
+        </section>
+
+        {/* Work Experience Section */}
+        <section className="cv-section" id="experience">
+          <h2>Work Experience</h2>
+          <form>
+            <Input type={"text"} name={"company name"} />
+            <Input type={"text"} name={"position"} />
+            <Input type={"textarea"} name={"responsibilities"} />
+            <Input type={"number"} name={"from"} />
+            <Input type={"number"} name={"to"} />
+            <div className="button-group">
+              <button type="button">Edit</button>
+              <button type="button">Save</button>
+            </div>
+          </form>
+        </section>
+
+        {/* Create Form Button */}
+        <button className="create-cv-button">Create CV!</button>
+      </main>
+    </>
   )
 }
 
---
+++ src/components/Input.jsx
@@ -0,0 +1,5 @@
+export function Input({type, name}) {
+    return (
+        <input type={type} placeholder={name}></input>
+    )
+}
